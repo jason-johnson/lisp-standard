45971d2b84677a6b5bd2a56564d23f64b78359af
jason <jason@johnson-net.ch>
2010-09-03 08:02:17 +0200

    BUGFIX: removed param defaults
    defgeneric doesn't support parameter defaults

--- collection/collection.lisp
+++ collection/collection.lisp
@@ -6,28 +6,32 @@
 (defgeneric put! (container key value)
   (:documentation "Put value at key"))
 
+;; TODO: I will probably want to create my own defgeneric that allows the defaults to be declared here, since this makes more sense.  If a method doesn't define any then
+;; TODO: what was defined in the defgeneric is used, but defmethod always overrides if specified.  I could create my own generic function object to ensure that I have
+;; TODO: have to do the minimal work.  That way the normal method detection (with the labmda list) works properly, and then I add the defaults to the method after the fact.
+
 (defgeneric remove (container item &key from-end test test-not start end count key)
   (:documentation "Remove item from collection"))
 
 (defgeneric remove! (container item &key from-end test test-not start end count key)
   (:documentation "Destructive form of remove"))
 
-(defgeneric remove-if (predicate container &key from-end (start 0) end count key)
+(defgeneric remove-if (predicate container &key from-end start end count key)
   (:documentation "Remove items from container for which predicate returns t"))
 
-(defgeneric remove-if! (predicate container &key from-end (start 0) end count key)
+(defgeneric remove-if! (predicate container &key from-end start end count key)
   (:documentation "Destructive form of remove-if"))
 
-(defgeneric remove-if-not (predicate container &key from-end (start 0) end count key)
+(defgeneric remove-if-not (predicate container &key from-end start end count key)
   (:documentation "Remove items from container for which predicate returns nil"))
 
-(defgeneric remove-if-not! (predicate container &key from-end (start 0) end count key)
+(defgeneric remove-if-not! (predicate container &key from-end start end count key)
   (:documentation "Destructive form of remove-if-not"))
 
-(defgeneric remove-duplicates (container &key (test #'eql) test-not (start 0) end from-end key)
+(defgeneric remove-duplicates (container &key test test-not start end from-end key)
   (:documentation "Remove duplicates from container"))
 
-(defgeneric remove-duplicates! (container &key (test #'eql) test-not (start 0) end from-end key)
+(defgeneric remove-duplicates! (container &key test test-not start end from-end key)
   (:documentation "Destructive form of remove-duplicates"))
 
 (defgeneric reverse (container)
