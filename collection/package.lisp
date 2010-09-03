a96cd982f5bde8e6632653d72e8f7defbe777a9f
jason <jason@johnson-net.ch>
2010-09-03 07:59:18 +0200

    collection: removed #:uncurry import

--- collection/package.lisp
+++ collection/package.lisp
@@ -3,7 +3,6 @@
 (defpackage #:standard.collection
   (:nicknames #:std.collection #:collection)
   (:use #:cl #:std.base)
-;  (:import-from #:std.functional #:uncurry) ;TODO: do we really need these functions?  Only used by the hash print/read code
   (:shadow #:get #:remove #:map)
   (:export
    #:get
