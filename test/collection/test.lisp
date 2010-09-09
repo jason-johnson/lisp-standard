9c87655252867344863db68694e0c13a353b9ee1
jason <jason@johnson-net.ch>
2010-09-09 07:45:23 +0200

    Implemented tests more concisely
    Now there is a base test method for each collection method and the individual tests just call that with their own symbols.

--- test/collection/test.lisp
+++ test/collection/test.lisp
@@ -11,71 +11,124 @@
    (setf -hash- (hash:copy #{1 a, 2 b, 3 c}))
    (setf -set- (set:copy #[a b c])))
   (:function
-   (put-test (make put! list f verify)
-	     (let* ((size (list:length list))
-		    (it (funcall make size)))
-	       (dotimes (i size)
-		 (funcall put! it i (list:get list i)))
-	       (let ((result (funcall f it)))
-		 (funcall verify it result))))))
+   (%test-get (get collection index expected)
+	      (ensure-same expected (funcall get collection index))))
+  (:function
+   (%test-put! (get put! collection index old new)
+	       (ensure-same old (funcall get collection index))
+	       (funcall put! collection index new)
+	       (ensure-same new (funcall get collection index))))
+  (:function
+   (%test-copy (copy collection)
+	       (let ((result (funcall copy collection))
+		     (*lift-equality-test* 'equalp))
+		 (ensure-same result collection)
+		 (ensure-null (eq result collection)))))
+  (:function
+   (%test-find (find item collection)
+	       (ensure-same item (funcall find item collection))))
+  (:function
+   (%test-find-if (find item collection)
+		  (ensure-same item (funcall find (lambda (i) (eql i item)) collection))))
+  (:function
+   (%test-find-if-not (find item collection)
+		      (ensure-same item (funcall find (lambda (i) (not (eql i item))) collection))))
+  (:function
+   (%test-remove^ (find remove^ item collection)
+		 (ensure-same item (funcall find item collection))
+		 (let ((result (funcall remove^ item collection)))
+		   (ensure-null (funcall find item result))
+		   (ensure-null (funcall find item collection))))) ; NOTE: This is not necassarily true.  E.g. if you use a vector and remove the last element, the vector will probably be unchanged
+  (:function
+   (%test-remove (find remove item collection)
+		 (ensure-same item (funcall find item collection))
+		 (let ((result (funcall remove item collection)))
+		   (ensure-null (funcall find item result))
+		   (ensure-same item (funcall find item collection))))))
 
 (addtest test-get
-  (flet ((ensure-it (collection index expected)
-	     (ensure-same expected (collection:get collection index))))
-    (ensure-it -list- 1 'b)
-    (ensure-it -vector- 1 'b)
-    (ensure-it -buffer- 1 'b)
-    (ensure-it -string- 1 #\b)
-    (ensure-it -hash- 2 'b)))
+  (flet ((test (collection index expected)
+	     (%test-get #'collection:get collection index expected)))
+    (test -list- 1 'b)
+    (test -vector- 1 'b)
+    (test -buffer- 1 'b)
+    (test -string- 1 #\b)
+    (test -hash- 2 'b)))
 
 (addtest test-put!
-  (flet ((ensure-it (collection index old new)
-	     (ensure-same old (collection:get collection index))
-	     (collection:put! collection index new)
-	     (ensure-same new (collection:get collection index))))
-      (ensure-it -list- 1 'b 'z)
-      (ensure-it -vector- 1 'b 'z)
-      (ensure-it -buffer- 1 'b 'z)
-      (ensure-it -string- 1 #\b #\z)
-      (ensure-it -hash- 2 'b 'z)))
+  (flet ((test (collection index old new)
+	   (%test-put! #'collection:get #'collection:put! collection index old new)))
+    (test -list- 1 'b 'z)
+    (test -vector- 1 'b 'z)
+    (test -buffer- 1 'b 'z)
+    (test -string- 1 #\b #\z)
+    (test -hash- 2 'b 'z)))
 
 (addtest test-copy
-  (flet ((ensure-it (collection)
-	   (let ((result (collection:copy collection))
-		 (lift:*lift-equality-test* 'equalp))
-	     (ensure-same result collection)
-	     (ensure-null (eq result collection)))))
-    (ensure-it -list-)
-    (ensure-it -array-)
-    (ensure-it -vector-)
-    (ensure-it -buffer-)
-    (ensure-it -string-)
-    (ensure-it -hash-)
-    (ensure-it -set-)))
+  (flet ((test (collection)
+	   (%test-copy #'collection:copy collection)))
+    (test -list-)
+    (test -array-)
+    (test -vector-)
+    (test -buffer-)
+    (test -string-)
+    (test -hash-)
+    (test -set-)))
 
-(deftestsuite standard-collection-buffer-test (standard-collection-test)
-  ()
-  (:function
-   (put-test-local (f verify)
-     (put-test
-      #'buffer:make
-      #'buffer:put!
-      -list-
-      f
-      verify))))
+(addtest test-find
+  (flet ((test (collection item)
+	   (%test-find #'collection:find item collection)))
+    (test -list- 'b)
+    (test -vector- 'b)
+    (test -buffer- 'b)
+    (test -string- #\b)))
+
+(addtest test-find-if
+  (flet ((test (collection item)
+	   (%test-find-if #'collection:find-if item collection)))
+    (test -list- 'b)
+    (test -vector- 'b)
+    (test -buffer- 'b)
+    (test -string- #\b)))
+
+(addtest test-find-if-not
+  (flet ((test (collection item)
+	   (%test-find-if-not #'collection:find-if-not item collection)))
+    (test -list- 'b)
+    (test -vector- 'b)
+    (test -buffer- 'b)
+    (test -string- #\b)))
 
 (addtest test-remove^
-  (put-test-local
-   (lambda (it)
-     (buffer:remove^ 'b it))
-   (lambda (it result)
-     (ensure-null (find 'b result))
-     (ensure-null (find 'b it)))))
+  (flet ((test (collection item)
+	   (%test-remove^ #'collection:find #'collection:remove^ item collection)))
+    (test -list- 'b)
+    (test -vector- 'b)
+    (test -buffer- 'b)
+    (test -string- #\b)))
 
 (addtest test-remove
-  (put-test-local
-   (lambda (it)
-     (buffer:remove 'b it))
-   (lambda (it result)
-     (ensure-null (find 'b result))
-     (ensure-same 'b (find 'b it)))))
+  (flet ((test (collection item)
+	   (%test-remove #'collection:find #'collection:remove item collection)))
+    (test -list- 'b)
+    (test -vector- 'b)
+    (test -buffer- 'b)
+    (test -string- #\b)))    
+
+(deftestsuite standard-collection-buffer-test (standard-collection-test)
+  ())
+
+(addtest test-get
+  (%test-get #'buffer:get -buffer- 1 'b))
+
+(addtest test-put!
+  (%test-put! #'buffer:get #'buffer:put! -buffer- 1 'b 'z))
+
+(addtest test-copy
+  (%test-copy #'buffer:copy -buffer-))
+
+(addtest test-remove^
+  (%test-remove^ #'buffer:find #'buffer:remove^ 'b -buffer-))
+
+(addtest test-remove
+  (%test-remove #'buffer:find #'buffer:remove 'b -buffer-))
