1d4752114414fd250da1fabb2199ff176354d7eb
jason <jason@johnson-net.ch>
2010-09-21 18:28:45 +0200

    test add-collection-tests fix: moved arg defaults
    Now the argument defaults are specified with the arguments themselves.  This simplifies the tests a little and made it possible to now express the reduce tests via the macro.

--- test/collection/test.lisp
+++ test/collection/test.lisp
@@ -96,26 +96,32 @@
     (ensure-same 1 (funcall count item result))
     (ensure (not (equalp original collection)))))
 
-(defmacro add-collection-tests (name local-test-fun target-test-funs extra-args collections default-args)
-  (flet ((as-symbol (&rest args)
+(defmacro add-collection-tests (name local-test-fun target-test-funs extra-args collections)
+  (flet ((strip-defaults (args)
+	   (mapcar
+	    (lambda (arg)
+	      (if (atom arg)
+		  arg
+		  (first arg)))
+	    args))
+	 (as-symbol (&rest args)
 	   (intern (string-upcase (apply #'format nil args)))))
-    `(flet ((test (collection ,@extra-args)
-	      (,local-test-fun ,@target-test-funs collection ,@extra-args)))
+    `(flet ((test (collection &optional ,@extra-args)
+	      (,local-test-fun ,@target-test-funs collection ,@(strip-defaults extra-args))))
        ,@(loop for c in collections
-	    collect (let (cc args collection)
+	    collect (let (cname args collection)
 		      (if (atom c)
 			  (setf
-			   cc c
-			   args default-args)
+			   cname c)
 			  (setf
-			   cc (first c)
+			   cname (first c)
 			   args (rest c)))
-		      (if (atom cc)
-			  (setf collection (as-symbol "-~a-" cc))
+		      (if (atom cname)
+			  (setf collection (as-symbol "-~a-" cname))
 			  (setf
-			   collection (second cc)
-			   cc (first cc)))
-		      `(addtest ,(as-symbol "test-~a-~a" name cc)
+			   collection (second cname)
+			   cname (first cname)))
+		      `(addtest ,(as-symbol "test-~a-~a" name cname)
 			(test ,collection ,@args)))))))
 
 (deftestsuite standard-collection-test ()
@@ -131,122 +137,105 @@
 
 (add-collection-tests
  get
- %test-get (#'std:get) (index expected)
- (list (array (list 0 1) 'b)  vector buffer (string 1 #\b) (hash 2 'b)) (1 'b))
+ %test-get (#'std:get) ((index 1) (expected 'b))
+ (list (array (list 0 1))  vector buffer (string 1 #\b) (hash 2)))
 
 (add-collection-tests
  put!
- %test-put! (#'std:get #'std:put!) (index old new)
-(list (array (list 0 1) 'b 'z) vector buffer (string 1 #\b #\z) (hash 2 'b 'z)) (1 'b 'z))
+ %test-put! (#'std:get #'std:put!) ((index 1) (old 'b) (new 'z))
+(list (array (list 0 1)) vector buffer (string 1 #\b #\z) (hash 2)))
 
 (add-collection-tests
  copy
  %test-copy (#'std:copy) ()
- (list array vector buffer string hash set) ())
+ (list array vector buffer string hash set))
 
-(flet ((test (collection &optional (fun #'+) (expected 6))
-	 (%test-reduce #'std:reduce collection fun expected nil)))
-  (addtest test-reduce-list
-    (test (list 1 2 3)))
-  (addtest test-reduce-array
-    (test (array:make '(2 2) :initial-contents '((1 2) (3 0)))))
-  (addtest test-reduce-vector
-    (test (vector:make 3 :adjustable t :initial-contents (list 1 2 3) :fill-pointer 3)))
-  (addtest test-reduce-buffer
-    (test (vector 1 2 3)))
-  (addtest test-reduce-string
-    (test -string- #'list (list (list (list #\a #\b) #\c) #\a)))
-  (addtest test-reduce-hash
-    (test (hash:copy #{a 1, b 2, c 3})))
-  (addtest test-reduce-set
-    (test (set:copy #[1 2 3]))))
+(add-collection-tests
+ reduce
+ %test-reduce (#'std:reduce) ((fun #'+) (expected 6) from-end)
+ (
+  ((list (list 1 2 3)))
+  ((array (array:make '(2 2) :initial-contents '((1 2) (3 0)))))
+  ((vector (vector:make 3 :adjustable t :initial-contents (list 1 2 3) :fill-pointer 3)))
+  ((buffer (vector 1 2 3)))
+  (string #'list (list (list (list #\a #\b) #\c) #\a))
+  ((hash (hash:copy #{a 1, b 2, c 3})))
+  ((set (set:copy #[1 2 3])))))
 
-(flet ((test (collection &optional (expected (list 'a 'b 'c 'a)) (fun #'cons))
-	 (%test-reduce-with-i-v #'std:reduce collection fun expected t nil)))
-  (addtest test-reduce-from-end-list
-    (test -list-))
-  (addtest test-reduce-from-end-array
-    (test -array-))
-  (addtest test-reduce-from-end-vector
-    (test -vector-))
-  (addtest test-reduce-from-end-buffer
-    (test -buffer-))
-  (addtest test-reduce-from-end-string
-    (test -string- (list #\a #\b #\c #\a)))
-  (addtest test-reduce-from-end-hash
-    (test -hash-))
-  (addtest test-reduce-from-end-set
-    (test -set-)))
+(add-collection-tests
+ reduce-from-end
+ %test-reduce-with-i-v (#'std:reduce) ((fun #'cons) (expected (list 'a 'b 'c 'a)) (from-end t) initial-value)
+ (list array vector buffer (string #'cons (list #\a #\b #\c #\a)) hash set))
 
 (add-collection-tests
  count
- %test-count (#'std:count) (item expected)
- (list array vector buffer (string #\a 2) hash (set 'a 1)) ('a 2))
+ %test-count (#'std:count) ((item 'a) (expected 2))
+ (list array vector buffer (string #\a) hash (set 'a 1)))
 
 (add-collection-tests
  count-if
- %test-count-if (#'std:count-if) (item expected)
- (list array vector buffer (string #\a 2) hash (set 'a 1)) ('a 2))
+ %test-count-if (#'std:count-if) ((item 'a) (expected 2))
+ (list array vector buffer (string #\a) hash (set 'a 1)))
 
 (add-collection-tests
  count-if-not
- %test-count-if-not (#'std:count-if-not) (item expected)
- (list array vector buffer (string #\a 2) hash (set 'a 1)) ('a 2))
+ %test-count-if-not (#'std:count-if-not) ((item 'a) (expected 2))
+ (list array vector buffer (string #\a) hash (set 'a 1)))
 
 (add-collection-tests
  find
- %test-find (#'std:find) (item)
- (list array vector buffer (string #\b) hash set) ('b))
+ %test-find (#'std:find) ((item 'b))
+ (list array vector buffer (string #\b) hash set))
 
 (add-collection-tests
  find-if
- %test-find-if (#'std:find-if) (item)
- (list array vector buffer (string #\b) hash set) ('b))
+ %test-find-if (#'std:find-if) ((item 'b))
+ (list array vector buffer (string #\b) hash set))
 
 (add-collection-tests
  find-if-not
- %test-find-if-not (#'std:find-if-not) (item)
- (list array vector buffer (string #\b) hash set) ('b))
+ %test-find-if-not (#'std:find-if-not) ((item 'b))
+ (list array vector buffer (string #\b) hash set))
 
 (add-collection-tests
  remove^
- %test-remove^ (#'std:find #'std:remove^) (item)
- (list vector buffer (string #\b)) ('b))
+ %test-remove^ (#'std:find #'std:remove^) ((item 'b))
+ (list vector buffer (string #\b)))
 
 (add-collection-tests
  remove
- %test-remove (#'std:find #'std:remove) (item)
- (list vector buffer (string #\b)) ('b))
+ %test-remove (#'std:find #'std:remove) ((item 'b))
+ (list vector buffer (string #\b)))
 
 (add-collection-tests
  remove-if^
- %test-remove-if^ (#'std:find-if #'std:remove-if^) (item)
- (list vector buffer (string #\b)) ('b))
+ %test-remove-if^ (#'std:find-if #'std:remove-if^) ((item 'b))
+ (list vector buffer (string #\b)))
 
 (add-collection-tests
  remove-if
- %test-remove-if (#'std:find-if #'std:remove-if) (item)
- (list vector buffer (string #\b)) ('b))
+ %test-remove-if (#'std:find-if #'std:remove-if) ((item 'b))
+ (list vector buffer (string #\b)))
 
 (add-collection-tests
  remove-if-not^
- %test-remove-if-not^ (#'std:find-if-not #'std:remove-if-not^) (item)
- (list vector buffer (string #\b)) ('b))
+ %test-remove-if-not^ (#'std:find-if-not #'std:remove-if-not^) ((item 'b))
+ (list vector buffer (string #\b)))
 
 (add-collection-tests
  remove-if-not
- %test-remove-if-not (#'std:find-if-not #'std:remove-if-not) (item)
- (list vector buffer (string #\b)) ('b))
+ %test-remove-if-not (#'std:find-if-not #'std:remove-if-not) ((item 'b))
+ (list vector buffer (string #\b)))
 
 (add-collection-tests
  remove-duplicates
- %test-remove-duplicates (#'std:count #'std:remove-duplicates) (item starting)
- (list array vector buffer (string #\a 2) hash (set 'a 1)) ('a 2))
+ %test-remove-duplicates (#'std:count #'std:remove-duplicates) ((item 'a) (starting 2))
+ (list array vector buffer (string #\a 2) hash (set 'a 1)))
 
 (add-collection-tests
  remove-duplicates^
- %test-remove-duplicates^ (#'std:count #'std:remove-duplicates^) (item starting)
- (((list (list 'a 'b 'a 'a 'c 'a)) 'a 4) array vector buffer (string #\a 2) hash (set 'a 1)) ('a 2))
+ %test-remove-duplicates^ (#'std:count #'std:remove-duplicates^) ((item 'a) (starting 2))
+ (((list (list 'a 'b 'a 'a 'c 'a)) 'a 4) array vector buffer (string #\a 2) hash (set 'a 1)))
 
 (deftestsuite standard-collection-buffer-test (standard-collection-test)
   ())
