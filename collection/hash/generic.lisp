6ca65f87fbefb335c647f0e39622cb9afbc45cf8
jason <jason@johnson-net.ch>
2010-09-24 08:29:46 +0200

    moved value collection functions to hash.values
    Also updated generic functions to point to the new location.

--- collection/hash/generic.lisp
+++ collection/hash/generic.lisp
@@ -11,31 +11,31 @@
 
 (defmethod std.collection:reduce (function (collection hash-table) &key key from-end start end (initial-value nil initial-value-p))
   (declare (ignore start end))
-  (apply #'reduce function collection :key key :from-end from-end (if initial-value-p (list :initial-value initial-value))))
+  (apply #'hash.values:reduce function collection :key key :from-end from-end (if initial-value-p (list :initial-value initial-value))))
 
 (defmethod std.collection:find (item (collection hash-table) &key from-end start end key test test-not)
   (declare (ignore from-end start end))
-  (find item collection :key key :test test :test-not test-not))
+  (hash.values:find item collection :key key :test test :test-not test-not))
 
 (defmethod std.collection:find-if (predicate (collection hash-table) &key from-end start end key)
   (declare (ignore from-end start end))
-  (find-if predicate collection key))
+  (hash.values:find-if predicate collection key))
 
 (defmethod std.collection:find-if-not (predicate (collection hash-table) &key from-end start end key)
   (declare (ignore from-end start end))
-  (find-if-not predicate collection key))
+  (hash.values:find-if-not predicate collection key))
 
 (defmethod std.collection:count (item (collection hash-table) &key from-end start end key test test-not)
   (declare (ignore from-end start end))
-  (count item collection :key key :test test :test-not test-not))
+  (hash.values:count item collection :key key :test test :test-not test-not))
 
 (defmethod std.collection:count-if (predicate (collection hash-table) &key from-end start end key)
   (declare (ignore from-end start end))
-  (count-if predicate collection key))
+  (hash.values:count-if predicate collection key))
 
 (defmethod std.collection:count-if-not (predicate (collection hash-table) &key from-end start end key)
   (declare (ignore from-end start end))
-  (count-if-not predicate collection key))
+  (hash.values:count-if-not predicate collection key))
 
 ;; NOTE: count ignored because it makes no sense.  If the user wants to remove all but one they can use remove-duplicates
 ;; (defmethod std.collection:remove^ (item (collection hash-table) &key from-end test test-not start end count key)
--- collection/hash/hash.lisp
+++ collection/hash/hash.lisp
@@ -57,60 +57,6 @@
 
 ;; These functions should go in the hash.values namespace.  Need to split out this code into files.  collection.lisp is for generic collection overrides, etc.  collection is last in dependencies, but in hash namespace so it can use fully qualified from
 ;; hash.values without depending on it in a use
-(defun reduce (function hash &key key from-end (initial-value nil initial-value-p))
-  (let ((last-result initial-value)
-	(has-value initial-value-p)
-	(f (if from-end
-	       (lambda (r v) (funcall function v r))
-	       function)))
-    (do ((nil value) hash last-result)
-	(let ((v (if key		; TODO: Would it be faster to set key to id by default and always call it?
-		     (funcall key value)
-		     value)))
-	      (setf last-result
-		    (if has-value
-			(funcall f last-result v)
-			v)
-		    has-value t)))))
-
-(defun count-if (predicate hash &optional key)
-  (let ((p (if key
-	       (compose predicate key)
-	       predicate))
-	(count 0))
-    (do ((nil value) hash count)
-	(when (funcall p value)
-	  (incf count)))))
-
-(defun count-if-not (predicate hash &optional key)
-  (count-if (compose #'not predicate) hash key))
-
-(defun count (item hash &key key test test-not)
-  (cond
-    (test (count-if (lambda (v) (funcall test v item)) hash key))
-    (test-not (count-if-not (lambda (v) (funcall test-not v item)) hash key))
-    (t (count-if (lambda (v) (eql v item)) hash key))))
-
-(defun find-if (predicate hash &optional key)
-  (let ((p (if key
-	       (compose predicate key)
-	       predicate)))
-    (do ((nil value) hash)
-	(when (funcall p value)
-	    (return-from find-if value)))))
-
-(defun find-if-not (predicate hash &optional key)
-  (find-if (compose #'not predicate) hash key))
-
-(defun find (item hash &key key test test-not)
-  (cond
-    (test (find-if (lambda (v) (funcall test v item)) hash key))
-    (test-not (find-if-not (lambda (v) (funcall test-not v item)) hash key))
-    (t (find-if (lambda (v) (eql v item)) hash key))))
-
-(defun-alias 'hash-table-count 'length)
-
-(defun-alias 'maphash 'map)
 
 ;; Read/write macros
 
