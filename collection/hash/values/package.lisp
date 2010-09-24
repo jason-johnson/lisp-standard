b97a4f3011c63a9bd6f36aef0de3c99bc3a4e378
jason <jason@johnson-net.ch>
2010-09-24 08:28:18 +0200

    hash.values: implemented #:do

--- collection/hash/values/package.lisp
+++ collection/hash/values/package.lisp
@@ -4,9 +4,10 @@
   (:nicknames #:std.collection.hash.values #:collection.hash.values #:hash.values)
   (:use #:cl #:std.collection.hash)
   (:import-from #:std.base #:defun-alias #:compose)
-  (:shadowing-import-from #:hash #:map #:do #:get #:length)
-  (:shadow #:count #:count-if #:count-if-not #:reduce #:find #:find-if #:find-if-not #:remove #:remove!)
+  (:shadowing-import-from #:hash #:map #:get #:length)
+  (:shadow #:do #:count #:count-if #:count-if-not #:reduce #:find #:find-if #:find-if-not #:remove #:remove!)
   (:export
+   #:do
    #:count
    #:count-if
    #:count-if-not
--- collection/hash/values/values.lisp
+++ collection/hash/values/values.lisp
@@ -1,1 +1,6 @@
 (in-package #:std.collection.hash.values)
+
+(defmacro do ((value hash &optional result) &body body) ;TODO: Needs a with-gensyms on hash
+  "Iterate over a hash's values"
+  `(hash:do ((nil ,value) ,hash ,result)
+	    ,@body))
