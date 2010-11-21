c85f6c773d0700ea12a4dbfe022445c9a9eaeb71
jason <jason@johnson-net.ch>
2010-11-21 02:10:11 +0100

    list:split^ functions: added start, end, count

--- collection/list/list.lisp
+++ collection/list/list.lisp
@@ -77,8 +77,9 @@
 (defun merge (list1 list2 predicate &key key)
   (merge^ (deep-copy list1) (deep-copy list2) predicate :key key))
 
-(defun split-if^ (predicate list &key key from-end start end count remove-empty)
-  (let ((first? t)
+(defun split-if^ (predicate list &key key from-end (start 0) end count remove-empty)
+  (let ((pushed 0)
+	(first? t)
 	(p (if key
 	       (compose predicate key)
 	       predicate))
@@ -87,14 +88,23 @@
     (flet ((push-last ()
 	     (unless (and remove-empty (not last))
 	       (if from-end (setf last (nreverse last)))
+	       (incf pushed)
 	       (push last result))))
       (if from-end (setf list (nreverse list)))
       (cl:do ((head list (rest head))
-	      (pointer (cons nil list) head))
-	     ((not head) (progn
-			   (push-last)
-			   (if from-end result (nreverse result))))
+	      (pointer (cons nil list) head)
+	      (current 0 (1+ current)))
+	     ((or
+	       (not head)
+	       (and count (= count pushed)))
+	      (progn
+		(push-last)
+		(if from-end result (nreverse result))))
 	(cond
+	  ((< current start))
+	  ((and end (= current end))
+	   (rplacd pointer nil)
+	   (setf head nil))
 	  ((funcall p (first head))
 	   (rplacd pointer nil)
 	   (push-last)
@@ -104,10 +114,10 @@
 	   (setf first? nil
 		 last head)))))))
 
-(defun split-if-not^ (predicate list &key key from-end start end count remove-empty)
+(defun split-if-not^ (predicate list &key key from-end (start 0) end count remove-empty)
   (split-if^ (complement predicate) list :key key :from-end from-end :start start :end end :count count :remove-empty remove-empty))
 
-(defun split^ (delimiter list &key key from-end start end count test test-not remove-empty)
+(defun split^ (delimiter list &key key from-end (start 0) end count test test-not remove-empty)
   (cond
     (test (split-if^ (lambda (v) (funcall test v delimiter)) list :key key :from-end from-end :start start :end end :count count :remove-empty remove-empty))
     (test-not (split-if-not^ (lambda (v) (funcall test-not v delimiter)) list :key key :from-end from-end :start start :end end :count count :remove-empty remove-empty))
