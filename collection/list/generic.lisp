d2361c33b2b359b244197266c04102654821d8b8
jason <jason@johnson-net.ch>
2010-11-26 07:33:57 +0100

    list: added generic split functions

--- collection/list/generic.lisp
+++ collection/list/generic.lisp
@@ -22,3 +22,12 @@
 
 (defmethod std.collection:merge ((output-spec (eql 'list)) (collection1 cons) (collection2 cons) predicate &key key)
   (merge collection1 collection2  predicate :key key))
+
+(defmethod std.collection:split (delimiter (collection list) &key key from-end (start 0) end count test test-not remove-empty)
+  (split delimiter collection :key key :from-end from-end :start start :end end :count count :test test :test-not test-not :remove-empty remove-empty))
+
+(defmethod std.collection:split-if (predicate (collection list) &key key from-end (start 0) end count remove-empty)
+  (split-if predicate collection :key key :from-end from-end :start start :end end :count count :remove-empty remove-empty))
+
+(defmethod std.collection:split-if-not (predicate (collection list) &key key from-end (start 0) end count remove-empty)
+  (split-if-not predicate collection :key key :from-end from-end :start start :end end :count count :remove-empty remove-empty))
