5c645d14d9882e45422e1a1781fbb59cd066e9c8
jason <jason@johnson-net.ch>
2012-05-23 23:34:36 +0200

    array - generic: fixed test variables to be function instead of symbol

--- collection/array/generic.lisp
+++ collection/array/generic.lisp
@@ -15,7 +15,7 @@
 (defmethod std.collection:length ((collection array))
   (length collection))
 
-(defmethod std.collection:count (item (collection array) &key from-end (start 0) end key (test 'eql))
+(defmethod std.collection:count (item (collection array) &key from-end (start 0) end key (test #'eql))
   (count item collection :from-end from-end :start start :end end :key key :test test))
 
 (defmethod std.collection:count-if (predicate (collection array) &key from-end (start 0) end key)
@@ -24,7 +24,7 @@
 (defmethod std.collection:reduce (function (collection array) &key key from-end (start 0) end (initial-value nil initial-value-p))
   (apply #'reduce function collection :key key :from-end from-end :start start :end end (if initial-value-p (list :initial-value initial-value))))
 
-(defmethod std.collection:find (item (collection array) &key from-end (start 0) end key (test 'eql))
+(defmethod std.collection:find (item (collection array) &key from-end (start 0) end key (test #'eql))
   (find item collection :from-end from-end :start start :end end :key key :test test))
 
 (defmethod std.collection:find-if (predicate (collection array) &key from-end (start 0) end key)
@@ -36,7 +36,7 @@
 (defmethod std.collection:position-if (predicate (collection array) &key from-end start end key)
   (position-if predicate collection :from-end from-end :start start :end end :key key))
 
-(defmethod std.collection:position (item (collection array) &key from-end start end key (test 'eql))
+(defmethod std.collection:position (item (collection array) &key from-end start end key (test #'eql))
   (position item collection :from-end from-end :start start :end end :key key :test test))
 
 (defmethod std.collection:substitute-if! (new predicate (collection array) &key from-end (start 0) end key count)
